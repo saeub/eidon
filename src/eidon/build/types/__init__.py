579ccc23d7733a323e38bec0402d48d7c13f2216
theDebbister <deborah@jakobis.net>
2026-06-22 16:02:08 +0200

    added tracking mode parameter

--- src/eidon/build/types/__init__.py
+++ src/eidon/build/types/__init__.py
@@ -3,8 +3,9 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any
+from typing import Any, Literal
 
+TrackingMode = Literal['remote', 'head-stabilized']
 
 @dataclass(kw_only=True)
 class ExperimentType(ABC):
@@ -18,12 +19,14 @@ class ExperimentType(ABC):
     area is within the trackable range of your eye-tracker. Note that it is not equivalent to the resolution of
     the monitor, but it cannot be greater than the resolution.
     :param margin_px: The margin in px. This margin will be deducted from the width and height of the stimulus area.
+    :param tracking_mode: Tracking mode of the eye-tracker. For example, remote tracking or head-stabilized.
     """
 
     # TODO: Use more user-friendly formats for color and size, and avoid list->tuple conversion for PIL
     stimulus_area_px: tuple[int, int]
     margin_px: int
     background_color: tuple[int, int, int] = (204, 204, 204)
+    tracking_mode: TrackingMode
 
     @classmethod
     def get_subclasses(cls) -> dict[str, type[ExperimentType]]:
